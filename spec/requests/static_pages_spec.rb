846343dcbe5d13dbcabe6b290ea45124617a38b6
mnorman343 <mnorman343@gmail.com>
2013-03-07 11:17:23 -0500

    add title page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,9 +21,14 @@ describe "Static pages" do
       page.should have_selector('h1', :text =>'Sample App')
     end
     
-    it "should have the title 'home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} home")
+      page.should have_selector('title', :text => "Tutorial")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => ": home")
     end
   end
   
